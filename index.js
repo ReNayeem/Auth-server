45fb1a57d29873de621302474f5574e802b28027
ReNayeem <renayeem@gmail.com>
2022-07-09 18:07:59 +0600

    Setup done

---
+++ index.js
@@ -0,0 +1,43 @@
+const express = require('express');
+const cors = require('cors');
+const { MongoClient, ServerApiVersion, ObjectId } = require('mongodb');
+require('dotenv').config();
+const port = process.env.PORT || 5000;
+const app = express();
+app.use(cors());
+app.use(express.json());
+
+
+const uri = "mongodb+srv://auth:2JaTCqPvdLXrR4jg@cluster0.2bsdr.mongodb.net/?retryWrites=true&w=majority";
+const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true, serverApi: ServerApiVersion.v1 });
+
+
+async function run() {
+  try {
+
+    await client.connect();
+    const userCollection = client.db('auth').collection('users');
+
+    //all users
+    app.get('/users', async (req, res) => {
+      const query = {};
+      const cursor = userCollection.find(query);
+      const items = await cursor.toArray();
+      res.send(items);
+    });
+  }
+  finally {
+
+  }
+
+}
+run().catch(console.dir);
+
+
+app.get('/', (req, res) => {
+  res.send('Auth server Running');
+});
+
+app.listen(port, () => {
+  console.log('Listening to port', port);
+})
